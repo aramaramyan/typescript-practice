b402f0f0b296b37fc2bfbe9890d79aea227f6435
aramaramyan <aramaramyan1997@mail.ru>
2022-07-15 16:44:34 +0400

    feat: add filter to render function and fix items duplicating

--- src/app.ts
+++ src/app.ts
@@ -131,7 +131,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(project => {
+        if(this.type === "active") {
+          return project.status === ProjectsStatus.Active;
+        }
+        return project.status === ProjectsStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -141,6 +147,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = "";
     for(const projectItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = projectItem.title;
