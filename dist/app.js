ed880a48a92e98a55b539d1ccc0ec74fb7571b0d
aramaramyan <aramaramyan1997@mail.ru>
2022-07-15 11:40:07 +0400

    feat: create ProjectList class and renderd active and finished lists

--- dist/app.js
+++ dist/app.js
@@ -35,6 +35,26 @@ function autoBind(_, _2, descriptor) {
     };
     return adjustedDescriptor;
 }
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById("project-list");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul").id = listId;
+        this.element.querySelector("h2").textContent = this.type.toUpperCase() + " PROJECTS";
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById("project-input");
@@ -101,3 +121,5 @@ __decorate([
     autoBind
 ], ProjectInput.prototype, "submitHandler", null);
 const projInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
